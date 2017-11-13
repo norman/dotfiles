e3b734ae14a553edc511c7184b44ca4375f6b5ec
Norman Clarke <norman@njclarke.com>
2017-11-13 11:47:10 -0300

    Don't die

--- install.rb
+++ install.rb
@@ -2,5 +2,10 @@ require "fileutils"
 
 files = Dir["*"] - [File.basename(__FILE__)]
 files.each do |file|
-  FileUtils.ln_s(File.expand_path(file), File.expand_path("~/.#{file}"))
+  begin
+    FileUtils.ln_s(File.expand_path(file), File.expand_path("~/.#{file}"))
+    puts "linked #{file}"
+  rescue Errno::EEXIST
+    puts "#{file} already linked, skipped"
+  end
 end
